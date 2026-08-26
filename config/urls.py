fc3736114aa540c333b5cc00f8393bb3f340f171
Kaneki <centipede.ghoul240@gmail.com>
2026-08-27 00:51:21 +0530

    Add custom 404 error page with AttendX branding

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.defaults import page_not_found
+
+# Custom error handlers
+def custom_404(request, exception):
+    return page_not_found(request, exception, template_name='404.html')
+
+handler404 = 'config.urls.custom_404'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
