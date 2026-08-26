13ca3470f14aa337ff44d07dd42525ac925d3ab2
Kaneki <centipede.ghoul240@gmail.com>
2026-08-27 02:52:21 +0530

    Remove temporary seed URL

--- config/urls.py
+++ config/urls.py
@@ -6,8 +6,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.defaults import page_not_found
-from apps.core.views_seed_students import seedstudents_view
-
 # Custom error handlers
 def custom_404(request, exception):
     return page_not_found(request, exception, template_name='404.html')
@@ -16,9 +14,6 @@ handler404 = 'config.urls.custom_404'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    # Temporary: Seed students
-    path('seedstudents/', seedstudents_view, name='seedstudents'),
     
     # Core app
     path('', include('apps.core.urls')),
